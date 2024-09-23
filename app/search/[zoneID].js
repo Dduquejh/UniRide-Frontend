8165af6490e384ff53547c8c2ded735203805377
Dduquejh <johandanielduquehernandez@gmail.com>
2024-09-22 20:40:08 -0500

    feat: Create page for share a trip or search a trip
    Create the page and the form if you want to publish a trip

--- app/search/[zoneID].js
+++ app/search/[zoneID].js
@@ -1,24 +1,62 @@
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 import { zones } from "../../constants";
 import TabBar from "../../components/TabBar";
+import { useState } from "react";
+import ShareForm from "../../components/ShareForm";
 
 const SearchZone = () => {
   const insets = useSafeAreaInsets();
   const { zoneID, communityID } = useLocalSearchParams();
   const zone = zones.find((z) => z.zoneID === zoneID);
+  const [isSharing, setIsSharing] = useState(true);
   return (
     <View
-      className="flex-1 w-4/5 justify-center mx-auto"
+      className="flex-1 w-full mx-auto items-center"
       style={{ marginTop: insets.top }}
     >
-      <View className="flex-1 w-4/5 justify-center items-center">
-        <Text>Options for {zone.text}</Text>
-        <Text>Building...</Text>
+      <View className="p-4 shadow-2xl mb-4 border-b-4 border-gray-200 w-full bg-gray-100 rounded-xl">
+        <Text className="text-2xl font-bold text-black">
+          Comunidad {communityID}
+        </Text>
       </View>
-      <TabBar communityID={communityID} />
+
+      <View className="w-4/5 mx-auto">
+        <View className="flex-row justify-around pb-2 border-b-2 border-gray-200">
+          <Pressable
+            asChild
+            className="flex items-center space-y-1 w-1/2"
+            onPress={() => setIsSharing(true)}
+          >
+            <Text className={`${isSharing ? "font-bold" : ""}`}>
+              Compartir mi vehículo
+            </Text>
+          </Pressable>
+
+          <Pressable
+            className="flex items-center space-y-1 w-1/2"
+            onPress={() => setIsSharing(false)}
+          >
+            <Text className={`${!isSharing ? "font-bold" : ""}`}>
+              Tomar un viaje
+            </Text>
+          </Pressable>
+        </View>
+
+        <View className="pt-6 mt-6 bg-slate-300 h-5/6 rounded-xl">
+          {isSharing ? (
+            <ShareForm />
+          ) : (
+            <Text>Información sobre tomar un viaje...</Text>
+          )}
+        </View>
+      </View>
+
+      <View className="w-4/5 mx-auto absolute bottom-0">
+        <TabBar communityID={communityID} />
+      </View>
     </View>
   );
 };
---
+++ components/ShareForm.js
@@ -0,0 +1,142 @@
+import { useState } from "react";
+import {
+  Alert,
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  ScrollView,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import Checkbox from "expo-checkbox";
+import { Picker } from "@react-native-picker/picker";
+
+const ShareForm = () => {
+  const [selectedCheckbox, setSelectedCheckbox] = useState(false);
+  const [date, setDate] = useState(new Date());
+  const [formattedDate, setFormattedDate] = useState(
+    // eslint-disable-next-line prettier/prettier
+    "Selecciona la fecha"
+  );
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [selectedHour, setSelectedHour] = useState("");
+  const [carSeats, setCarSeats] = useState(1);
+  const [description, setDescription] = useState("");
+  const [isPressed, setIsPressed] = useState(false);
+
+  const handleDateChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShowDatePicker(false);
+    setDate(currentDate);
+
+    const dateString = currentDate.toLocaleDateString();
+    setFormattedDate(dateString);
+  };
+
+  const handleSubmit = () => {
+    Alert.alert(
+      "Tu viaje ha sido publicado con éxito!",
+      "Recuerda actualizar el número de cupos disponibles desde tu perfil. Los interesados en tu viaje te contactaran por WhatsApp.",
+      [
+        {
+          text: "OK",
+        },
+        // eslint-disable-next-line prettier/prettier
+      ]
+    );
+    console.log("Form submitted");
+    // Implement form submission logic here
+  };
+
+  const hourOptions = [];
+  for (let hour = 6; hour <= 20; hour++) {
+    hourOptions.push(`${hour < 10 ? `0${hour}` : hour}:00`);
+    hourOptions.push(`${hour < 10 ? `0${hour}` : hour}:30`);
+  }
+
+  return (
+    <ScrollView className="w-4/5 mx-auto">
+      <View>
+        <View className="flex-row items-center">
+          <Checkbox
+            value={selectedCheckbox === "From"}
+            onValueChange={() => setSelectedCheckbox("From")}
+          />
+          <Text className="pl-2 text-lg">Desde la universidad</Text>
+        </View>
+        <View className="flex-row items-center">
+          <Checkbox
+            value={selectedCheckbox === "To"}
+            onValueChange={() => setSelectedCheckbox("To")}
+          />
+          <Text className="pl-2 text-lg">Hacia la universidad</Text>
+        </View>
+        <View>
+          <Text className="font-bold text-xl pt-4">Salida</Text>
+          <Pressable onPress={() => setShowDatePicker(true)}>
+            <Text className="text-base font-semibold bg-white rounded-xl pl-4">
+              {formattedDate}
+            </Text>
+          </Pressable>
+          {showDatePicker && (
+            <DateTimePicker
+              value={date}
+              mode="date"
+              display="default"
+              onChange={handleDateChange}
+            />
+          )}
+
+          <Text className="text-base font-semibold pt-2">
+            Selecciona la hora
+          </Text>
+          <View className="bg-white rounded-xl justify-center h-6">
+            <Picker
+              selectedValue={selectedHour}
+              onValueChange={(itemValue) => setSelectedHour(itemValue)}
+            >
+              {hourOptions.map((hour) => (
+                <Picker.Item key={hour} label={hour} value={hour} />
+              ))}
+            </Picker>
+          </View>
+        </View>
+
+        <Text className="text-xl font-bold pt-4">Cupos disponibles</Text>
+        <View className="bg-white rounded-xl justify-center h-6">
+          <Picker
+            selectedValue={carSeats}
+            onValueChange={(itemValue) => setCarSeats(itemValue)}
+          >
+            {[1, 2, 3, 4, 5].map((seats) => (
+              <Picker.Item key={seats} label={seats.toString()} value={seats} />
+            ))}
+          </Picker>
+        </View>
+
+        <Text className="text-xl font-bold pt-4 pb-2">
+          Descripción del trayecto
+        </Text>
+        <TextInput
+          value={description}
+          onChangeText={setDescription}
+          placeholder="Describe tu trayecto"
+          className="bg-white rounded-xl px-4 h-24"
+        />
+
+        <Pressable
+          onPress={handleSubmit}
+          onPressIn={() => setIsPressed(true)}
+          onPressOut={() => setIsPressed(false)}
+          className={`mt-4 h-12 justify-center items-center rounded-xl ${
+            isPressed ? "bg-teal-400" : "bg-teal-700"
+          }`}
+        >
+          <Text className="text-white font-semibold">Ofrecer un viaje</Text>
+        </Pressable>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default ShareForm;
