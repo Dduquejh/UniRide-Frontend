55bd582e1a94d771a04d034269c5b5947f6b3b64
Dduquejh <johandanielduquehernandez@gmail.com>
2024-09-26 09:42:03 -0500

    feat: Organice page and connect form with database
    Use axios and make a post petition with info of the form. ToDo: show
    message of errors, for example when the password didn't have the
    neccesary things

--- app/signin/[communityID].js
+++ app/signin/[communityID].js
@@ -1,20 +1,73 @@
-import { useState } from "react";
-import { useLocalSearchParams } from "expo-router";
-import { Image, Text, TextInput, View } from "react-native";
+import { useContext, useState } from "react";
+import { router, useLocalSearchParams } from "expo-router";
+import {
+  ActivityIndicator,
+  Alert,
+  Image,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { Link } from "expo-router";
+import { CommunitiesContext } from "../../components/GetCommunities";
+import Constants from "expo-constants";
 
-import { communities } from "../../constants";
 import CustomButton from "../../components/CustomButton";
+import axios from "axios";
 
 export default function Login() {
-  const { communityID } = useLocalSearchParams();
-  const community = communities.find((c) => c.communityID === communityID);
-  const imageSource = community ? community.imageSource : null;
-
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { communityID } = useLocalSearchParams();
+  const { communities, loading, error } = useContext(CommunitiesContext);
+  if (loading) {
+    return (
+      <View className="w-4/5 justify-center items-center">
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+  if (error) {
+    return (
+      <View className="w-4/5 justify-center items-center">
+        <Text className="text-2xl font-bold text-red-600">
+          Error: {error.message}
+        </Text>
+      </View>
+    );
+  }
+
+  const community = communities.find((c) => c.id === communityID);
+  const imageSource = community ? community.imageSource : null;
+
+  const handleRegister = async () => {
+    if (!name || !phone || !email || !password) {
+      Alert.alert("Error", "Por favor, llene todos los campos");
+      return;
+    }
+    try {
+      const url = Constants.expoConfig.extra.apiUrl;
+      const userData = {
+        fullName: name,
+        phone: phone,
+        email: email,
+        password: password,
+      };
+      const response = await axios.post(`${url}/auth`, userData);
+      if (response.status === 201) {
+        Alert.alert("Éxito", "Usuario registrado exitosamente");
+        router.push(`/login/${communityID}`);
+      } else if (response.status === 400) {
+        Alert.alert("Error", response.data.message);
+      }
+    } catch (e) {
+      console.log(e);
+      Alert.alert("Error", "Error al registrar usuario");
+    }
+  };
+
   return (
     <View className="w-4/5 justify-center">
       <View className="flex-row items-center mb-8 bg-teal-700 rounded-xl justify-center w-full">
@@ -23,7 +76,7 @@ export default function Login() {
           {community.text}
         </Text>
         <Image
-          source={imageSource}
+          source={{ uri: imageSource }}
           style={{ width: 100, height: 100 }}
           resizeMode="contain"
         />
@@ -61,9 +114,8 @@ export default function Login() {
           onChangeText={setPassword}
           className="mb-4 border-b-2 border-gray-200 w-full h-10 px-2"
         />
-        <Link asChild href={`../login/${communityID}`}>
-          <CustomButton text="Sign In" />
-        </Link>
+
+        <CustomButton text="Sign In" onPress={handleRegister} />
       </View>
     </View>
   );
