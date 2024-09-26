e46a1f20764bde446d827c8f5ba7ae4214eac8bf
Dduquejh <johandanielduquehernandez@gmail.com>
2024-09-26 10:17:49 -0500

    feat: Connect login with database
    Use axios to make a post petition and make the login process

--- app/login/[communityID].js
+++ app/login/[communityID].js
@@ -1,18 +1,72 @@
-import { useState } from "react";
-import { useLocalSearchParams } from "expo-router";
-import { Image, Text, TextInput, View } from "react-native";
-import { Link } from "expo-router";
+import { useContext, useState } from "react";
+import { router, useLocalSearchParams } from "expo-router";
+import {
+  ActivityIndicator,
+  Alert,
+  Image,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
-import { communities } from "../../constants";
+import { CommunitiesContext } from "../../components/GetCommunities";
+import Constants from "expo-constants";
 import CustomButton from "../../components/CustomButton";
+import axios from "axios";
 
 export default function Login() {
-  const { communityID } = useLocalSearchParams();
-  const community = communities.find((c) => c.communityID === communityID);
-  const imageSource = community ? community.imageSource : null;
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { communityID } = useLocalSearchParams();
+  const { communities, loading, error } = useContext(CommunitiesContext);
+  if (loading) {
+    return (
+      <View className="w-4/5 justify-center items-center">
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+  if (error) {
+    return (
+      <View className="w-4/5 justify-center items-center">
+        <Text className="text-2xl font-bold text-red-600">
+          Error: {error.message}
+        </Text>
+      </View>
+    );
+  }
+  const community = communities.find((c) => c.id === communityID);
+  const imageSource = community ? community.imageSource : null;
+
+  const handleLogin = async () => {
+    if (!email || !password) {
+      Alert.alert("Error", "Por favor, llene todos los campos");
+      return;
+    }
+    try {
+      const url = Constants.expoConfig.extra.apiUrl;
+      const userData = {
+        email: email,
+        password: password,
+      };
+      const response = await axios.post(`${url}/auth/login`, userData);
+      if (response.status === 201) {
+        router.push(`../zone/${communityID}?token=${response.data.token}`);
+        console.log(response.data.token);
+      } else if (response.status === 400) {
+        Alert.alert("Error", response.data.message);
+      }
+      if (response.status === 404) {
+        Alert.alert("Error", "dsa");
+        console.log(response.data.message);
+      }
+    } catch (e) {
+      console.log(e);
+      console.log(e.message);
+      Alert.alert("Error", "Error al ingresar");
+    }
+  };
+
   return (
     <View className="w-4/5 justify-center">
       <View className="flex-row items-center mb-8 bg-teal-700 rounded-xl justify-center w-full">
@@ -21,7 +75,7 @@ export default function Login() {
           {community.text}
         </Text>
         <Image
-          source={imageSource}
+          source={{ uri: imageSource }}
           style={{ width: 100, height: 100 }}
           resizeMode="contain"
         />
@@ -43,9 +97,8 @@ export default function Login() {
           onChangeText={setPassword}
           className="mb-4 border-b-2 border-gray-200 w-full h-10"
         />
-        <Link asChild href={`../zone/${communityID}`}>
-          <CustomButton text="Log In" />
-        </Link>
+
+        <CustomButton text="Log In" onPress={handleLogin} />
       </View>
     </View>
   );
